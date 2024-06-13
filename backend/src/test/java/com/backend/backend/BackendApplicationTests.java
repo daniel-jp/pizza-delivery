ded020e0ec0548c123848a8f77ee55a83d0e1d33
daniel-jp <danny198672@hotmail.com>
2024-06-13 23:08:06 +0100

    Dockerfile added

--- backend/src/test/java/com/backend/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/backend/backend/BackendApplicationTests.java
@@ -3,7 +3,9 @@ package com.backend.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.backend.pizza.BackendApplication;
+
+@SpringBootTest(classes = BackendApplication.class)
 class BackendApplicationTests {
 
 	@Test
